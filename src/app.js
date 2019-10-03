6e82c38bd2747e54ba25c66b81711981e3178a94
lfresnog <luisfresno99@gmail.com>
2019-10-03 12:46:50 +0200

    fouth commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 console.log("Exito");
+import yargs from 'yargs';
+
+yargs.command({
+    command:'localization',
+    describe:'Indicate the weather in a zone',
+    builder:{
+        name:{
+            describe:'Name of the localization',
+            demandOption:true,
+            type:'string',
+        },
+        index: {
+            describe: 'Posible places in that place',
+            demandOption: true,
+            type: 'number',
+          },
+    },
+    handler: localization,
+})
