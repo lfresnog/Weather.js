7a01f0889a4ed7db16d4550f1d8ec64b4007f270
lfresnog <luisfresno99@gmail.com>
2019-10-03 14:02:17 +0200

    six commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
-console.log("Exito");
 import yargs from 'yargs';
+import request from 'request';
 
-yargs.command({
-    command:'localization',
-    describe:'Indicate the weather in a zone',
-    builder:{
-        name:{
-            describe:'Name of the localization',
-            demandOption:true,
-            type:'string',
-        },
-        index: {
-            describe: 'Posible places in that place',
-            demandOption: true,
-            type: 'number',
-          },
-    },
-    handler: localization,
-})
+const url =
+  'https://api.darksky.net/forecast/7e42fcf88c4ed04bbdd548e6c7469798/40.28,3.43?units=si&lang=es';
+
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.currently.temperature);
+});
+
+console.log('Hello world!!');
