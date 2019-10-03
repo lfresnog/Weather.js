3e2a0162d50e0163c5c2fce44d14472b6177059a
lfresnog <luisfresno99@gmail.com>
2019-10-03 17:27:24 +0200

    seven commit

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,6 @@ const localization = function localization(argv){
         const mapBoxtoken = 'pk.eyJ1IjoibGZyZXNub2ciLCJhIjoiY2sxYW8wc3o2MGczbTNpcjVoZ2sxbWRnZiJ9.Jdss3fcdT5Nypqn2KMQiFA';
         const config = '&language=es'
         const mapBoxurl = `${mapBoxURL}${city}${mapBoxtoken}${config}`;
-        console.log(mapBoxurl);
         if (argv.index==null){
             request({ url: mapBoxurl, json: true }, (error, response) => {
                 console.log("\n Las ciudades con ese nombre son:\n")
@@ -23,15 +22,14 @@ const localization = function localization(argv){
         request({ url: mapBoxurl, json: true }, (error, response) => {
             console.log("");
             
-                console.log(`   ${response.body.features[argv.index].place_name}`);
-                console.log(chalk.yellow(`       x: ${response.body.features[argv.index].geometry.coordinates[0]}  y: ${response.body.features[argv.index].geometry.coordinates[1]}`));
+                console.log(`${response.body.features[argv.index].place_name}`);
+                console.log(chalk.yellow(`  x: ${response.body.features[argv.index].geometry.coordinates[0]}  y: ${response.body.features[argv.index].geometry.coordinates[1]}`));
 
               const cordX = response.body.features[argv.index].geometry.coordinates[0];
               const cordY = response.body.features[argv.index].geometry.coordinates[1];
               const darkSkyURl = 'https://api.darksky.net/forecast/';
               const darkSkytoken = '7e42fcf88c4ed04bbdd548e6c7469798';
               const darkSkyurl = `${darkSkyURl}${darkSkytoken}/${cordX},${cordY}`;
-              console.log("\n" + darkSkyurl);
               request({ url: darkSkyurl, json: true }, (error, response) => {
               console.log(`La temperatura en esa ciudad es ${response.body.currently.temperature}`);
               });
